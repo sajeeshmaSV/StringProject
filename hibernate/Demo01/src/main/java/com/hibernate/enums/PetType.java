a2dc3038177f39a65d9a36cdce50fd4b32a89e45
sajeeshmaSV <sajeesajeeshmasv@gmail.com>
2026-03-26 23:52:59 +0530

    Delete hibernate directory

--- hibernate/Demo01/src/main/java/com/hibernate/enums/PetType.java
+++
@@ -1,6 +0,0 @@
-package com.hibernate.enums;
-
-public enum PetType {
-    Bird,Dogs,Cat,Tiger,Lion
-
-}
--- hibernate/Demo13/src/main/java/com/hibernate/crud/App.java
+++
@@ -1,7 +0,0 @@
-package com.hibernate.crud;
-
-public class App {
-    public static void main() {
-
-    }
-}
