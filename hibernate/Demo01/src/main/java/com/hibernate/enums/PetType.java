b3ac845c982f5239b4c451a42f061e7e3d422e99
sajeeshmaSV <sajeesajeeshmasv@gmail.com>
2026-03-26 23:31:53 +0530

    Add files via upload

---
+++ hibernate/Demo01/src/main/java/com/hibernate/enums/PetType.java
@@ -0,0 +1,6 @@
+package com.hibernate.enums;
+
+public enum PetType {
+    Bird,Dogs,Cat,Tiger,Lion
+
+}
---
+++ hibernate/Demo13/src/main/java/com/hibernate/crud/App.java
@@ -0,0 +1,7 @@
+package com.hibernate.crud;
+
+public class App {
+    public static void main() {
+
+    }
+}
