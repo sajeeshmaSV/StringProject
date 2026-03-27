340c9fee9c81fce19de293fc97440fb2d9acfca2
sajeeshmaSV <sajeesajeeshmasv@gmail.com>
2026-03-27 22:05:51 +0530

    Delete SpringBoot-main/Demo 1 directory

--- SpringBoot-main/Demo 1/src/main/java/com/springboot/Demo/repository/Studentrepository.java
+++
@@ -1,6 +0,0 @@
-
-package com.springboot.respository;
-
-public interface StudentRepository {
-    String printRegNo(int regNo);
-}
--- SpringBoot-main/Demo 1/src/main/java/com/springboot/Demo/service/Studentservice.java
+++
@@ -1,6 +0,0 @@
-
-package com.springboot.service;
-
-public interface StudentService {
-    String printRegNo(int regNo) ;
-}
